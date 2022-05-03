515fb4de58beaf5a427173b3aa1888233b001d6b
차용준 <zauin09@gmail.com>
2022-05-04 06:27:28 +0900

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,33 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: env('SENDGRID_FROM_EMAIL'),
+        defaultReplyTo:  env('SENDGRID_REPLYTO_EMAIL'),
+        testAddress:  env('SENDGRID_TEST_EMAIL'),
+      },
+    },
+  },
+  // ...
+
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+
+});
